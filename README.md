70fd9dca2a308b215ee49279308e97929a2b66ce
Nanderson Castro <nandokstro@gmail.com>
2018-01-16 20:33:46 -0300

    Iniciando projeto do curso API com Symfony.

---
+++ README.md
@@ -0,0 +1,4 @@
+api_ecommerce
+=============
+
+A Symfony project created on January 16, 2018, 8:36 pm.
---
+++ src/ApiBundle/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ApiBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+    	return new JsonResponse(['msg' => 'Hello World'], 200);
+    }
+}
